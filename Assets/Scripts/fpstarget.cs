d65ecd33bd023c040513c6b9db3beb888f740350
KyleJordan <kjpatrick40@gmail.com>
2021-02-16 20:09:53 -0500

    Official Commit 1 by Kyle: basic movement, placeholder sprites for the player character and motus, jumping.

---
+++ Assets/Scripts/fpstarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpstarget : MonoBehaviour
+{
+    public int targetFrameRate = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = targetFrameRate;
+    }
+
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D bc;
+    public float speed = 10;
+    public float jumpForce = 9;
+    public LayerMask ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (bc.IsTouchingLayers(ground))
+                Jump(Vector2.up);
+        }
+
+        Walk(dir);
+    }
+
+    private void Walk(Vector2 dir)
+    {
+        rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
+    }
+
+    private void Jump(Vector2 dir)
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity += dir * jumpForce;
+    }
+}

cc46daf8ce93d78f00a5775e2f632020121793bb
Anthony <atran776@gmail.com>
2021-02-21 01:19:40 -0500

    I am a god

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private AudioSource _audio;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audio = GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    public void PlayMusic()
+    {
+        if (_audio.isPlaying) return;
+        _audio.Play();
+    }
+
+    // Update is called once per frame
+    public void StopMusic()
+    {
+        _audio.Stop();
+    }
+}
